97af1ed556f88d1bad0b4425cfa136fd10ff7e19
Josh Mutus <josh.mutus@gmail.com>
2019-02-17 16:19:17 -0800

    Add some tests

--- move_even_odd.py
+++ move_even_odd.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import os
+import re
 import sys
 import argparse
 import shutil
@@ -14,28 +15,39 @@ def list_files(path):
   onlyfiles = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
   return onlyfiles
 
-def move_file(filename, path):
+def move_file(filename, old_path, new_path):
   """Move a file to a relative path."""
-  if not os.path.exists(path):
-    os.makedirs(path)
-  og_loc = os.path.join(os.getcwd(), filename)
-  new_loc = os.path.join(path, filename)
+  if not os.path.exists(new_path):
+    os.makedirs(new_path)
+  og_loc = os.path.join(old_path, filename)
+  new_loc = os.path.join(new_path, filename)
   shutil.move(og_loc, new_loc)
+
+def create_paths(root_path):
+  """Creates input and output paths."""
+  odd_dir = os.path.join(root_path,'odd')
+  even_dir = os.path.join(root_path, 'even')
+  return odd_dir, even_dir
+
+def get_file_no(fname):
+  """Returns the number label of a given image file."""
+  res = re.findall(r'\d+', fname)
+  return int(res[0])
 
 def main(argv):
   parser = argparse.ArgumentParser()
-  parser.add_argument('--output')
+  parser.add_argument('--path')
   args = parser.parse_args()
   print(args)
-  print(list_files(os.getcwd()))
-  file_list = list_files(os.getcwd())
-  odd_dir = os.path.join(os.getcwd(),'odd')
-  even_dir = os.path.join(os.getcwd(), 'even')
-  for idx, f in enumerate(file_list):
-    if idx%2 == 0:
-      move_file(f, even_dir)
+  print(args.path)
+  print(list_files(args.path))
+  file_list = list_files(args.path)
+  odd_dir, even_dir = create_paths(args.path)
+  for f in file_list:
+    if get_file_no(f)%2 == 0:
+      move_file(f, args.path, even_dir)
     else:
-      move_file(f, odd_dir)
+      move_file(f, args.path, odd_dir)
 
 if __name__ == '__main__':
   main(sys.argv)
---
+++ move_even_odd_test.py
@@ -0,0 +1,30 @@
+"""Tests for move_even_odd."""
+import pytest
+import move_even_odd
+import os
+import shutil
+
+@pytest.fixture(scope="session")
+def make_files(tmp_path):
+  print('setting up')
+  test_path = os.path.join(os.getcwd(),'test')
+  if not os.path.exists(test_path):
+    os.makedirs(test_path)
+  for i in range(101):
+    open(os.path.join(test_path,'IMG{}.foo'.format(i)),'a').close()
+  return test_path
+
+# def teardown_function(make_files):
+#   print(make_files)
+#   shutil.rmtree(make_files)
+#   print('tearing down')
+
+def test_get_file_no():
+  fn = "IMG11122233.jpeg"
+  num = move_even_odd.get_file_no(fn)
+  assert num == 11122233
+
+
+def test_create_new_paths(make_files):
+  print(make_files)
+  assert 1 == 1
